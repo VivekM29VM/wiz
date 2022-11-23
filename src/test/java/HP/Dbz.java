2dd8dadf1c2d8d8b1a6a1428ab14fe1fa5eb4ced
DELL <DELL@Vivek>
2022-11-23 17:47:43 +0530

    ok

--- src/test/java/HP/Dbz.java
+++ src/test/java/HP/Dbz.java
@@ -7,17 +7,18 @@ public class Dbz {
 	@Test
 	public void earth() {
 		
-		System.out.println("Goku god");
+		System.out.println("Goku ");
 		
 		
 		System.out.println("Vegeta");
 		
 		
-		System.out.println("Broly");
+	
 		
 		
-		
-		System.out.println("Cumber");
 	}
 
 }
+	
+		
+		
