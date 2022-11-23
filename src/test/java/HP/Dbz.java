019f6500f688eb7296e14d1c666385159071b747
DELL <DELL@Vivek>
2022-11-23 15:43:26 +0530

    done

---
+++ src/test/java/HP/Dbz.java
@@ -0,0 +1,19 @@
+package HP;
+
+import org.testng.annotations.Test;
+
+public class Dbz {
+	
+	@Test
+	public void earth() {
+		
+		System.out.println("Goku");
+		
+		System.out.println("Vegeta");
+		
+		System.out.println("Broly");
+		
+		System.out.println("Cumber");
+	}
+
+}
